19a4617bff37566077c0a777ca9d17d9709b1c03
Patrick Leonardus <patrickleonardus@gmail.com>
2019-06-29 10:09:51 +0700

    Initial Commit

---
+++ Aqurate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aqurate
+//
+//  Created by Patrick Leonardus on 29/06/19.
+//  Copyright © 2019 Patrick Leonardus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
